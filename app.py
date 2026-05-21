09a9596dff691475d26dbddad86f86d0fa3a3d6d
M.Saad.Khan <133247609+Saadhere123@users.noreply.github.com>
2026-05-22 01:49:41 +0500

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,17 @@ import streamlit as st
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 import torch
 
-# Load from HuggingFace
 model_name = "saadhere4705/news-topic-bert"
 
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForSequenceClassification.from_pretrained(model_name)
+
+labels = {
+    0: "World",
+    1: "Sports",
+    2: "Business",
+    3: "Sci/Tech"
+}
 
 st.title("News Topic Classifier 📰")
 
@@ -20,4 +26,4 @@ if st.button("Predict"):
 
     prediction = torch.argmax(outputs.logits, dim=1).item()
 
-    st.write("Predicted Class:", prediction)
+    st.success(f"Predicted Topic: {labels[prediction]}")
